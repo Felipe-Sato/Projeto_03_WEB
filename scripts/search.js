e966d177f230cbe2a1b36a3f297236f6d85e7df1
Felipe Sato <felipe.sato270@gmail.com>
2022-04-27 22:24:54 -0300

    Update search.js
    Set debug point to verify the response of the API

--- scripts/search.js
+++ scripts/search.js
@@ -1,12 +1,23 @@
 var button = document.querySelector("#submit"),
     termo,
-    texto = document.querySelector(".response");
+    ul = document.querySelector("ul");
 
 button.addEventListener("click", async function() {
     termo = document.querySelector("#search").value;
     // Implementação da consulta AJAX com API Fetch
     let Body = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/"+termo);
     let obj = await Body.json();
-    console.log(obj);
-    texto.innerHTML = obj.word;
+    for (var i = 0; i < obj.length; i++) {
+        var li = document.createElement("li");
+            li.innerHTML = obj[i].word;
+            console.log(obj[i].word);
+            console.log(obj[i].meanings);
+            console.log(obj[i].license);
+            /*
+        for (var j = 0; j < obj[i].meanings.length; j) {
+            li.innerHTML = obj[i].meanings[j];
+            console.log(li.innerHTML);
+        }
+        */
+    }
 })
