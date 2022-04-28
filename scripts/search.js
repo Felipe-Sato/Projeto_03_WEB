82ab85b71c5725a3dcd986487cb0c1faaa3f6c31
Felipe Sato <felipe.sato270@gmail.com>
2022-04-28 15:09:29 -0300

    Update search.js
    Updated the layout of how the response would be show on the main screen;
    
    Added auto-clear the response box before new searches;
    
    Added function to the clean button on the page.

--- scripts/search.js
+++ scripts/search.js
@@ -1,39 +1,99 @@
-var button = document.querySelector("#submit"),
+var submit = document.querySelector("#submit"),
+    reset = document.querySelector("#reset"),
     termo,
-    ul = document.querySelector("ul");
+    response = document.querySelector(".response");
+    /*
+    word = document.querySelector(".word"),
+    phonetic = document.querySelector(".phoenetic"),
+    license_name = document.querySelector(".license_name"),
+    license_url = document.querySelector(".license_url")
+    ol = document.querySelector("ol");*/
 
-button.addEventListener("click", async function() {
+submit.addEventListener("click", async function() {
+    // Clear response box
+    response.innerHTML = '';
     termo = document.querySelector("#search").value;
-    // Implementação da consulta AJAX com API Fetch
+
+    // Implementação da consulta AJAX com API Fetch e Async e Await
     let Body = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/"+termo);
     let obj = await Body.json();
-    for (var i = 0; i < obj.length; i++) {
-        var li = document.createElement("li");
-            li.innerHTML = obj[i].word;
-            ul.appendChild(li);
-            console.log(obj[i].license.name);
-            console.log(obj[i].license.url);
 
-        for (var j = 0; j < obj[i].meanings.length; j++) {
-            console.log(obj[i].meanings[j].partOfSpeech);
-            li.innerHTML = obj[i].meanings[j].partOfSpeech;
-            ul.appendChild(li);
-            li.innerHTML = obj[i].meanings[j].synonyns;
-            ul.appendChild(li);
-            li.innerHTML = obj[i].meanings[j].antonyns;
-            ul.appendChild(li);
+    /* ====== Estruturas da resposta ====== */
+    //word
+    var word = document.createElement("p");
+    word.className = "word";
+    word.innerHTML = obj[0].word;
+    response.appendChild(word);
 
-            for (var k = 0; k < obj[i].meanings[j].definitions.length; k++) {
-                console.log(obj[i].meanings[j].definitions[k].definition)
-                li.innerHTML = obj[i].meanings[j].definitions[k].definition;
-                ul.appendChild(li);
-                li.innerHTML = obj[i].meanings[j].definitions[k].synonyns;
-                ul.appendChild(li);
-                li.innerHTML = obj[i].meanings[j].definitions[k].antonyns;
-                ul.appendChild(li);
-                li.innerHTML = obj[i].meanings[j].definitions[k].example;
-                ul.appendChild(li);
-            }
-        }
+    //phonetic
+    if(obj[0].phonetic != undefined) {
+        var phonetic = document.createElement("p");
+        phonetic.className = "phonetic";
+        response.appendChild(phonetic);
     }
+
+    //meanings
+    var meaning = document.createElement("ol");
+    meaning.className = "meaning";
+    for (var i = 0; i < obj[0].meanings.length; i++) {
+        var outer_li = document.createElement("li");
+        var partOfSpeech = document.createElement("ul");
+        partOfSpeech.className = "partOfSpeech";
+        partOfSpeech.innerHTML = obj[0].meanings[i].partOfSpeech;
+        var inner_li = document.createElement("li");
+
+        var ol = document.createElement("ol");
+        ol.className = "definitions";
+
+        for (var j = 0; j < obj[0].meanings[i].definitions.length; j++) {
+            var definition = document.createElement("li");
+            definition.className = "definition"
+            definition.innerHTML = obj[0].meanings[i].definitions[j].definition;
+
+            var ul = document.createElement("ul");
+
+            if (obj[0].meanings[i].definitions[j].synonyns != null) {
+                var synonyns = document.createElement("li");
+                synonyns.className = "synonyns";
+                synonyns.innerHTML = obj[0].meanings[i].definitions[j].synonyns;
+                ul.appendChild(synonyns)
+            }
+
+            if (obj[0].meanings[i].definitions[j].antonyns != null) {
+                var antonyns = document.createElement("li");
+                antonyns.className = "antonyns";
+                antonyns.innerHTML = obj[0].meanings[i].definitions[j].antonyns;
+                ul.appendChild(antonyns);
+            }
+
+            var example = document.createElement("li");
+            example.className = "example";
+            example.innerHTML = obj[0].meanings[i].definitions[j].example;
+            ul.appendChild(example);
+
+            definition.appendChild(ul);
+            ol.appendChild(definition);
+            inner_li.appendChild(ol);
+        }
+        partOfSpeech.appendChild(inner_li);
+        outer_li.appendChild(partOfSpeech);
+        meaning.appendChild(outer_li);
+    }   
+    
+    response.appendChild(meaning);
+
+    // license
+    var license_name = document.createElement("p");
+        license_name.className = "license";
+        license_name.innerHTML = obj[0].license.name;
+        response.appendChild(license_name);
+
+        var license_url = document.createElement("p");
+        license_url.className = "license";
+        license_url.innerHTML = obj[0].license.url;
+        response.appendChild(license_url);
+})
+
+reset.addEventListener("click", function(){
+    response.innerHTML = '';
 })
