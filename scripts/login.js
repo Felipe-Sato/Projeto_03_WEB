32f39c28f9b35ac70dea025a830961e3e2c63cbb
AntonioNigro1 <46200602+AntonioNigro1@users.noreply.github.com>
2022-05-08 17:59:49 -0300

    Update login.js
    Regras de erro revisadas/adicionadas

--- scripts/login.js
+++ scripts/login.js
@@ -8,21 +8,31 @@ document.getElementById('signup').addEventListener('click', function(){
 })
 
 function verificaEmail(emailinput){
-
+    if(emailinput.value.indexOf("@") == -1 || emailinput.value.indexOf("@")==0 || emailinput.value.substring(-emailinput.length)=='@' || emailinput.value.length <=3){ 
+        document.getElementById('erroemail').innerHTML = "Erro, email invalido";
+        document.getElementById('erroemail').classList.toggle('displaynone', false);
+        return false;
+    }
     let usuario = emailinput.value.slice(0, emailinput.value.indexOf("@"));
     let dominio = emailinput.value.slice(emailinput.value.indexOf("@")+1, emailinput.value.length);
 
-    if(usuario!=null && dominio!=null) return true;
-    else if (usuario == null || dominio == null) return false;
-    
+    if(usuario!=null, undefined, '', ' ' && dominio!=null, undefined,' ','') return true;
+    else if (usuario == null || dominio == null){
+        document.getElementById('erroemail').innerHTML = "Erro, email invalido";
+        document.getElementById('erroemail').classList.toggle('displaynone', false);
+        return false;
+    }
 }
 
 function verificaSenha(pswinput){
     senha = pswinput.value;
 
-    if(senha.length >= 6) return true; 
-    else return false;
-    
+    if(senha.length >= 3) return true; 
+    else{ 
+        document.getElementById('errosenha').innerHTML = "Erro, senha invalido";
+        document.getElementById('errosenha').classList.toggle('displaynone', false);
+        return false;
+    }
 }
 //para testar o registro(signup) temos de usar o usuario definido pela API (para testes)
 //podendo escolher qualquer senha para o registro
@@ -40,6 +50,14 @@ document.getElementById('signup2').addEventListener('click', async function(emai
             password:pswinput.value})
             }
         )
+        document.getElementById('errosenha').classList.toggle('displaynone', true);
+        document.getElementById('erroemail').classList.toggle('displaynone', true);
+    }else if(!verificaEmail(emailinput)){
+        document.getElementById('erroemail').innerHTML = "Erro, email invalido";
+        document.getElementById('erroemail').classList.toggle('displaynone', false);
+    }else if (!verificaSenha(pswinput)){
+        document.getElementById('errosenha').innerHTML = "Erro, senha invalido";
+        document.getElementById('errosenha').classList.toggle('displaynone', false);
     }
 })
 
