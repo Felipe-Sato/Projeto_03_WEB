303d8e73295d9dbd8937c9581406fe79f1e81811
Felipe Sato <felipe.sato270@gmail.com>
2022-06-10 21:43:27 -0300

    Update Word_Router.mjs
    Created word router class.

--- app/routes/Word_Router.mjs
+++ app/routes/Word_Router.mjs
@@ -1,1 +1,19 @@
 import WordDAO from './app/models/Words';
+
+import http from 'http'
+import express from 'express'
+
+// Classe para o roteamento das requisicoes de Palavras
+module.exports = class WordRouter {
+    static async find(key) {
+        return Client.connect('mongodb://localhost:27017/words', async (err, db) => {
+            return db.collection('words').find(key).toArray();
+        });
+    }
+
+    static async post(key) {
+        return Client.connect('mongodb://localhost:27017/words', async (err, db) => {
+            return db.collection('words').insertOne(key);
+        });
+    }
+}
