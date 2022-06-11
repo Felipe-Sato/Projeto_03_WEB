7366e0ebee5f932c8c1c1422d22990ad67313290
Felipe Sato <felipe.sato270@gmail.com>
2022-06-10 21:42:46 -0300

    Update Users.mjs
    Create User model for mongoDB.

--- app/models/Users.mjs
+++ app/models/Users.mjs
@@ -1,6 +1,31 @@
-import {MongoClient as Client} from 'mongodb';
+import mongoose from 'mongoose';
 
 // Classe de acesso ao banco de dados dos usuarios
-module.exports = class User {
+const user = new mongoose.Schema({
+    "email": {
+        type: "string",
+        required: true,
+        unique: true,
+        max: 255,
+        min: 0
+    },
 
-};
+    "password": {
+        type: "string",
+        required: true,
+        max: 255,
+        min: 0
+    },
+
+    "photo": {
+        type: "file",
+        required: true,
+    },
+
+    "adminLevel": {
+        type: "boolean",
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', user);
