37125a90a41e3fb6e8bc9d3b9ccaff516e93876e
Felipe Sato <felipe.sato270@gmail.com>
2022-06-10 21:42:26 -0300

    Update Words.mjs
    Created Word model for mongoDB.

--- app/models/Words.mjs
+++ app/models/Words.mjs
@@ -1,6 +1,48 @@
-import {MongoClient as Client} from 'mongodb';
+import mongoose from 'mongoose';
 
 // Classe de acesso ao banco de dados das palavras
-module.exports = class Word {
+const word = new mongoose.Schema({
+    "word": {
+        type: "string",
+        required: true,
+        max: 64,
+        min: 0
+    },
 
-};
+    "partOfSpeech": {
+        type: "string",
+        required: false,
+        max: 64,
+        min: 0
+    },
+
+    "definition": {
+        type: "string",
+        required: false,
+        max: 255,
+        min: 0
+    },
+
+    "example": {
+        type: "string",
+        required: true,
+        max: 255,
+        min: 0
+    },
+
+    "synonyms": {
+        type: "string",
+        required: false,
+        max: 255,
+        min: 0
+    },
+
+    "antonyms": {
+        type: "string",
+        required: false,
+        max: 255,
+        min: 0
+    }
+});
+
+module.exports = mongoose.model('Word', word);
