9df8dc022898cbf3e6fd92928ae1be24d84ab18a
Felipe Sato <felipe.sato270@gmail.com>
2022-06-10 21:59:03 -0300

    Update Models
    Remodeled words and users models

--- app/models/Users.mjs
+++ app/models/Users.mjs
@@ -1,31 +1,16 @@
-import mongoose from 'mongoose';
+import {MongoClient as Client} from 'mongodb';
 
 // Classe de acesso ao banco de dados dos usuarios
-const user = new mongoose.Schema({
-    "email": {
-        type: "string",
-        required: true,
-        unique: true,
-        max: 255,
-        min: 0
-    },
-
-    "password": {
-        type: "string",
-        required: true,
-        max: 255,
-        min: 0
-    },
-
-    "photo": {
-        type: "file",
-        required: true,
-    },
-
-    "adminLevel": {
-        type: "boolean",
-        required: true
+module.exports = class User {
+    static async find(key) {
+        return Client.connect('mongodb://localhost:27017/projeto3', async (err, db) => {
+            return db.collection('users').find(key).toArray();
+        });
     }
-});
 
-module.exports = mongoose.model('User', user);
+    static async post(key) {
+        return Client.connect('mongodb://localhost:27017/projeto3', async (err, db) => {
+            return db.collection('users').insertOne(key);
+        });
+    }
+};
--- app/models/Words.mjs
+++ app/models/Words.mjs
@@ -1,48 +1,16 @@
-import mongoose from 'mongoose';
+import {MongoClient as Client} from 'mongodb';
 
 // Classe de acesso ao banco de dados das palavras
-const word = new mongoose.Schema({
-    "word": {
-        type: "string",
-        required: true,
-        max: 64,
-        min: 0
-    },
-
-    "partOfSpeech": {
-        type: "string",
-        required: false,
-        max: 64,
-        min: 0
-    },
-
-    "definition": {
-        type: "string",
-        required: false,
-        max: 255,
-        min: 0
-    },
-
-    "example": {
-        type: "string",
-        required: true,
-        max: 255,
-        min: 0
-    },
-
-    "synonyms": {
-        type: "string",
-        required: false,
-        max: 255,
-        min: 0
-    },
-
-    "antonyms": {
-        type: "string",
-        required: false,
-        max: 255,
-        min: 0
+module.exports = class Word {
+    static async find(key) {
+        return Client.connect('mongodb://localhost:27017/projeto3', async (err, db) => {
+            return db.collection('words').find(key).toArray();
+        });
     }
-});
 
-module.exports = mongoose.model('Word', word);
+    static async post(key) {
+        return Client.connect('mongodb://localhost:27017/projeto3', async (err, db) => {
+            return db.collection('words').insertOne(key);
+        });
+    }
+}
