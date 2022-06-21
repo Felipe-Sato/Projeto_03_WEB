c578f4af6829cf185e3e0a192bb28a72bed7e7b7
Felipe Sato <felipe.sato270@gmail.com>
2022-06-21 11:36:53 -0300

    Update index.mjs
    Inserted Routers to index.mjs API

--- index.mjs
+++ index.mjs
@@ -1,18 +1,14 @@
 import express from 'express';
 import http from 'http';
 import path from 'path';
+const User_Router = require('./app/routes/UserRouter.mjs');
+const Word_Router = require('./app/routes/WordRouter.mjs');
 
 // App.Set
 app.set('views', path.join(__dirname, 'app.views'));
 app.set('view engine', 'hbs');
 
 // Node.js Middleware
-app.use();
-
-// Main App
-var app = express(),
-    port;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -23,5 +19,10 @@ app.use(cors());
 
 app.use('/user', User_Router);
 app.use('/word', Word_Router);
+
+// Main App
+var app = express(),
+    port;
+
 
 http.createServer(app).listen(port);
