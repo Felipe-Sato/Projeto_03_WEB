c0a705e8b2af17d9bdd5b5769cf9c5f6eb8d86d0
Felipe Sato <felipe.sato270@gmail.com>
2022-06-22 16:32:16 -0300

    Create UsersController.js
    Added controller for Sign In and Sign Up of users to the database.

---
+++ app/controllers/UsersController.js
@@ -0,0 +1,51 @@
+const User = require('../models/Users');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'as34a';
+
+module.exports = {
+    get : async (req, res) => {
+        const { token } = req.query;
+        const { email } = req.query;
+
+        try {
+            // Auth token
+            jwt.verify(token, JWT_SECRET);
+            
+            // Search for user
+            const reply = await User.find({email: email});
+    
+            // Error treatment
+            if  (reply.length === 0) {
+                res.status(404).json({ status: '404', error: '404 Not Found' });
+            } else {
+                res.status(200).json({ status: '200', data: reply });
+            }
+        } catch (err) {
+            res.status(401).json({ status: '401', error: '401 Not Authenticaded' });
+        }
+    },
+    post : async (req, res) => {
+        const { token } = req.query;
+        const { nome, email, senha, admin} = req.body;
+        
+        try {
+            // Auth token
+            jwt.verify(token, JWT_SECRET);
+                
+            // Create new User
+            const reply = await User.createElement({
+                nome: nome, 
+                email: email, 
+                senha: senha, 
+                admin: admin
+            }, function (err, reply) {
+                if (err) {
+                    res.status(400); // Not created
+                    throw err;
+                }
+            });
+        } catch (err) {
+            res.status(401).json({ status: '401', error: '401 Not Authenticaded' });
+        }
+    }
+}
