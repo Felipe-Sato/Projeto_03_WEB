69b0624ffe28ceff1420c5ed0941fec2d801c382
Felipe Sato <felipe.sato270@gmail.com>
2022-06-22 16:34:20 -0300

    Update Front-End
    Added fields and buttons to sign up and sign in and to search and insertion of words to the database.

--- app/public/scripts/control.js
+++ app/public/scripts/control.js
@@ -22,7 +22,27 @@ function logado(){
 document.getElementById('btnlogoff').addEventListener('click',function(){
     document.getElementById('btnlogoff').classList.add('displaynone');
     localStorage.removeItem('token');
-
     logado();
-})
+});
 
+document.querySelector('#btn-form-signup').addEventListener('click',function(){
+    document.querySelector('#user_name').classList.toggle('displaynone', false);
+});
+
+document.querySelector('#btn-form-signin').addEventListener('click',function(){
+    document.querySelector('#user_name').classList.toggle('displaynone', true);
+});
+
+document.querySelector('#btn-form-insert').addEventListener('click',function(){
+    document.querySelector('#type').classList.toggle('displaynone', false);
+    document.querySelector('#text').classList.toggle('displaynone', false);
+    document.querySelector('#search').classList.toggle('displaynone', true);
+    document.querySelector('#insert').classList.toggle('displaynone', false);
+});
+
+document.querySelector('#btn-form-search').addEventListener('click',function(){
+    document.querySelector('#type').classList.toggle('displaynone', true);
+    document.querySelector('#text').classList.toggle('displaynone', true);
+    document.querySelector('#search').classList.toggle('displaynone', false);
+    document.querySelector('#insert').classList.toggle('displaynone', true);
+});
