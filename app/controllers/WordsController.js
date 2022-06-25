01bfc2589878b5de76ab7988b7b91295ed08b536
Felipe Sato <felipe.sato270@gmail.com>
2022-06-25 16:46:01 -0300

    Update WordsController

--- app/controllers/WordsController.js
+++ app/controllers/WordsController.js
@@ -9,7 +9,7 @@ module.exports = {
 
         try {
             // Search engine by Word
-            const reply = await Word.find({ word: word }).toArray();
+            const reply = await Word.find({ word: word });
             // Error treatment
             if (reply != null) {
                 console.log(reply);
