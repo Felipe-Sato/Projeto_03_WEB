b9367343f3cb1a9a6974bc35c1f4b9ce2b1e674c
Felipe Sato <felipe.sato270@gmail.com>
2022-06-26 15:10:35 -0300

    Test GET Word

--- app/public/scripts/getWord.js
+++ app/public/scripts/getWord.js
@@ -8,7 +8,10 @@ search.addEventListener("click", async function () {
     var termo = document.querySelector("#termo").value;
 
     // Implementação da consulta AJAX com API Fetch e Async e Await
-    let obj = await fetch('https://web-api-dicionario-simples.herokuapp.com/Words/'+ termo);
+    let obj = await fetch('https://web-api-dicionario-simples.herokuapp.com/Words/'+ termo, {
+        method: 'GET',
+        mode: 'cors'
+    });
     if (obj.status === 200) {
         console.log(obj);
         /* ====== Estruturas da resposta ====== */
